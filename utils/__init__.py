6456d0545c9c972dcf7c3f0faa8111b87976d88a
Seyong Kang <nettie0917@gmail.com>
2024-04-24 16:00:58 +0000

    init
    init repositoy

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+"""General helper utilities the workshop notebooks"""
+# Python Built-Ins:
+from io import StringIO
+import sys
+import textwrap
+
